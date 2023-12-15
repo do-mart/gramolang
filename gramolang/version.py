5cfc192536d4305dc2d512213d520527aba9d8a5
do-mart <dominic.martin@gmail.com>
2023-12-15 14:49:26 -0500

    Versions information update

--- gramolang/version.py
+++ gramolang/version.py
@@ -3,11 +3,14 @@ Versioning
 """
 
 
-VERSIONS: dict[str:str] = {
+DESCRIPTIONS: dict[str:str] = {
     '0.4': "Log starts here with new console class and simplified chat class",
     '0.4.1': "Implementation of Role enum, and Message and Completion NamedTuples",
     '0.5': "Integration of OpenAI API ver. 1.x",
-    '0.6': "New wraipi (wrapper of APIs) layer of abstraction over different APIs"
+    '0.6': "New wraipi (wrapper of AI organizations APIs) layer of abstraction",
+    '0.6.1': "Integration of a version control system (VCS) and code clean up",
+    '0.6.2': "Consolidation and bug fix for API keys retrieval methods",
+    '0.6.3': "Simplification of commands nomenclature",
 }
 
-VERSION = list(VERSIONS)[-1]
+VERSION = list(DESCRIPTIONS)[-1]
