a89c5a1a5af48178c1a29ae280d6ace82f6aa964
do-mart <dominic.martin@gmail.com>
2023-12-30 17:54:38 -0500

    Simplified API keys implementation and examples modules

--- gramolang/__init__.py
+++ gramolang/__init__.py
@@ -1,7 +1,6 @@
 """
 Package Initialization
 
-TODO: Change API key mechanism to only use hard key or environment variable.
 TODO: Use configparser to retrieve key from file and load in environment
 TODO: Move root modules in new examples package with a common module for the
       key import mechanism.
