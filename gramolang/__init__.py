7d2d0da409f74919c36a6c9b8bfeb2b5087351ba
do-mart <dominic.martin@gmail.com>
2023-12-31 11:01:54 -0500

    TODOs cleanup

--- gramolang/__init__.py
+++ gramolang/__init__.py
@@ -1,11 +1,7 @@
-"""
-Package Initialization
+"""Package Initialization"""
 
-TODO: Use configparser to retrieve key from file and load in environment
-TODO: Move root modules in new examples package with a common module for the
-      key import mechanism.
-TODO: Create complete in text file chat for one conversation based on sheet?
-"""
+# TODO: Create complete in text file chat for one conversation based on sheet?
+
 
 # Forwards
 from .common import NAME, NAME_VERSION
